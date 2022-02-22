8f00cdedccbf263e4b796ccb841ed283ad4ab9f8
Julia <juliagrainger745@gmail.com>
2022-02-21 23:42:02 -0600

    adding header details

--- koko-website1/curious-koko-nifties/src/components/Header.js
+++ koko-website1/curious-koko-nifties/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Header.css'
 import kokoLogo from '../assets/hawaiianshirtkoko.jpg'
+import searchIcon from '../assets/searchIcon.jpg'
 
 const Header = () => {
   return (
     <div className='header'>
-
       <div className='logoContainer'>
           <img src={kokoLogo} className="kokoLogo" alt="" />
       </div>
@@ -13,9 +13,14 @@ const Header = () => {
       <div className='searchContainer'>
         <div className='iconContainer'>
           <img src= {searchIcon} />
-
         </div>
+        <input 
+            className='searchInput' 
+            placeholder='Collection, item or user ...'
+        />
       </div>
     </div>
   )
 }
+
+export default Header
